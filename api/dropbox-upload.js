dee151b88f8345e27ced6afebd553c5bc100a33a
dennisemig <dennisemig@gmail.com>
2026-04-29 15:11:47 +0200

    Add files via upload

--- api/dropbox-upload.js
+++ api/dropbox-upload.js
@@ -1,3 +1,10 @@
+export const config = {
+  api: {
+    bodyParser: false,
+    responseLimit: false,
+  },
+}
+
 export default async function handler(req, res) {
   res.setHeader('Access-Control-Allow-Origin', '*')
   res.setHeader('Access-Control-Allow-Methods', 'POST, OPTIONS')
@@ -32,5 +39,3 @@ export default async function handler(req, res) {
     res.status(500).json({ error: e.message })
   }
 }
-
-export const config = { api: { bodyParser: false } }
